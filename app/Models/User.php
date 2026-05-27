6bf5305e1ed2380f593ec9d68e9796a02345698e
John Rene Bandong <bandongjr26@gmail.com>
2026-05-28 00:20:47 +0800

    Fix User model to use user_accounts table

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,55 +2,46 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    // 👇 Tell Laravel to use your custom table
+    protected $table = 'user_accounts';
+
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'role',
+        'is_active',
+        'must_change_password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-
     }
+
     public function profile()
     {
         return $this->hasOne(Profile::class);
     }
-    public function posts(){
+
+    public function posts()
+    {
         return $this->hasMany(Post::class);
     }
 }
